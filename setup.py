4a1f1bc3f8f515198984a8fdca23a9d12a7f02cb
Matthew Rocklin <mrocklin@gmail.com>
2015-07-31 09:29:51 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from os.path import exists
+from setuptools import setup
+
+setup(name='cachey',
+      version='0.0.1',
+      description='Caching mindful of computation/storage costs',
+      url='http://github.com/mrocklin/cachey/',
+      maintainer='Matthew Rocklin',
+      maintainer_email='mrocklin@gmail.com',
+      license='BSD',
+      keywords='',
+      packages=['cachey'],
+      install_requires=list(open('requirements.txt').read().strip().split('\n')),
+      long_description=(open('README.md').read() if exists('README.md')
+                        else ''),
+      zip_safe=False)
