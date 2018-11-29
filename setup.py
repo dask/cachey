5ec5405ca093e7e273705c8889e9cbb54c0bdc45
Yu Feng <rainwoodman@gmail.com>
2018-11-29 12:59:34 -0800

    support python setup.py pytest

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@
 from os.path import exists
 from setuptools import setup
 
+import sys
+if {'pytest', 'test', 'ptr'}.intersection(sys.argv):
+    setup_requires = ['pytest-runner']
+else:
+    setup_requires = []
+
 setup(name='cachey',
       version='0.1.1',
       description='Caching mindful of computation/storage costs',
@@ -12,7 +18,9 @@ setup(name='cachey',
       license='BSD',
       keywords='',
       packages=['cachey', 'cachey.tests'],
+      tests_requires=['pytest'],
       install_requires=list(open('requirements.txt').read().strip().split('\n')),
       long_description=(open('README.md').read() if exists('README.md')
                         else ''),
+      setup_requires=setup_requires,
       zip_safe=False)
